824c684eddb6d2bbbca7a3c31cf61c35aac11884
dodkirua <pybouttefeux@gmail.com>
2021-04-19 14:32:08 +0200

    ChatRoomManager.php finis

--- php/Classes/Manager/ChatRoomManager.php
+++ php/Classes/Manager/ChatRoomManager.php
@@ -44,9 +44,28 @@ class ChatRoomManager{
         return null;
     }
 
+    /**
+     * delete a chatroom
+     * @param int $id
+     * @return bool
+     */
     public function delChatRoom(int $id) : bool    {
         $request = DB::getInstance()->prepare("DELETE FROM chat_room WHERE id = :id");
         $request->bindValue(':id',$id);
         return $request->execute();
+    }
+
+    /**
+     * add a chatroom
+     * @param string $name
+     * @return bool
+     */
+    public function addChatRoom(string $name): bool {
+        $request = DB::getInstance()->prepare('
+            INSERT INTO chat_room (name)
+            VALUES (:name)');
+        $request->bindValue(':name',$name);
+        $request->execute();
+        return intval(DB::getInstance()->lastInsertId()) !==0;
     }
 }
